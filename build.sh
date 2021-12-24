0befb462777e0a0a6a3f91bc0d487fac1dccbaa5
mattragoza <mtr22@pitt.edu>
2021-12-24 11:31:46 -0500

    translate environment.yaml to meta.yaml (WIP)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake .. -DOPENBABEL3_INCLUDE_DIR=$PREFIX/include/openbabel3 -DOPENBABEL3_LIBRARIES=$PREFIX/lib/libopenbabel.so
-make -j8
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DOPENBABEL3_INCLUDE_DIR=$PREFIX/include/openbabel3 -DOPENBABEL3_LIBRARIES=$PREFIX/lib/libopenbabel.so
+make -j$CPU_COUNT
 make install
 
